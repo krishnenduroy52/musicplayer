b6a7072914c6638e48df41fb6b6ff5c79aedfe39
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-01-27 12:26:11 +0530

    song Update

--- all_songs/songs.js
+++ all_songs/songs.js
@@ -26,54 +26,5 @@ let all_songs = [
         path: "Ogochhalo_Mon_Taalpatar_Shepai",
         singer: "Taalpatar Shepai",
         img: "ogochhalo_mon"
-    },
-    {
-        id: "5",
-        name: "Ogochhalo Mon",
-        path: "Ogochhalo_Mon_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "6",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "7",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "8",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "9",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "10",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
-    },
-    {
-        id: "11",
-        name: "Music 4",
-        path: "Moshader_Chumu_Taalpatar_Shepai",
-        singer: "Taalpatar Shepai",
-        img: "ogochhalo_mon"
     }
 ]
